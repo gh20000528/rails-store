3f84d14cce26388670f678cfc4cf6c3fa9eb11a8
han sheng <gh890528@gmail.com>
2023-04-09 15:44:25 +0800

    store crud

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,7 +1,51 @@
 class StoresController < ApplicationController
+    before_action :find_store, only: [:edit, :update, :show, :destroy]
     def index
+        @stores = Store.all
+    end
+    def new
+        @store = Store.new
+    end
+    def create
+        @store = Store.new(store_params)
+        if @store.save
+            redirect_to stores_path
+        else
+            render :new
+        end
+    end
+    def show
         
     end
+    def edit
+        
+    end
+    def update
+        if @store.update(store_params)
+            redirect_to stores_path
+        else
+            render :edit
+        end
+    end
+    def destroy
+        @store.destroy
+        redirect_to stores_path
+    end
+
+    private
+    def store_params
+        params.require(:store).permit(:title , :tel , :address)
+    end
+    def find_store
+        @store = Store.find_by(id: params[:id])
+    end
+    
+    
+    
+    
+    
+    
+    
     
 
 end
